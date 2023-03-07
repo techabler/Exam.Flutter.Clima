4c670b966ced573483fcbc4e8eaa539db0c50ad9
ygpark <ygpark@hn-tech.co.kr>
2023-03-07 22:13:05 +0900

    -- get geolocation -- get weather by API

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+import '../services/location.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -6,13 +10,48 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  // NOTE : No.2 - geo location 패키지를 사용해서 사용자 위치값을 가져오자.
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    getData();
+    getLocation();
+    super.initState();
+  }
+
+  // NOTE : No.6 - 비동기 통신을 통한 위치값 가져오기
+  void getLocation() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+
+    print(location.latitude);
+    print(location.longitude);
+  }
+
+  // NOTE : No.8 - API를 통한 Web Data 가져오기
+  void getData() async {
+    String apiKey = "901860e65a73d3611ab4ff7f4dc0355d";
+    String strUri =
+        "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=$apiKey";
+    var url = Uri.parse(strUri);
+    http.Response response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+      print(data);
+    } else {
+      print(response.statusCode);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: RaisedButton(
+        child: ElevatedButton(
           onPressed: () {
-            //Get the current location
+            getLocation();
           },
           child: Text('Get Location'),
         ),
